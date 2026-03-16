bfdef3064a7dd7fbfb117f0fa9dbadfbf163bb05
DaeBreak Productions <daebreakproductions@gmail.com>
2026-03-16 01:45:46 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Toaster } from "@/components/ui/toaster"
-import { QueryClientProvider } from '@tanstack/react-query'
+import { QueryClientProvider, useQuery } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { base44 } from '@/api/base44Client';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
