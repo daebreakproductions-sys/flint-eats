4b57fffe5f06ce3f2092473853c889576db02a27
DaeBreak Productions <daebreakproductions@gmail.com>
2026-03-16 00:17:22 +0000

    File changes

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -58,8 +58,8 @@ export default function Map() {
         style={{ height: "100%" }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
         />
         <MarkerClusterGroup chunkedLoading>
           {filtered.map(resource => {
