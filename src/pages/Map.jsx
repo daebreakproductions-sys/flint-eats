5ab0e593bf63f8886867c88f3d1fa1c8a0f2defa
DaeBreak Productions <daebreakproductions@gmail.com>
2026-03-16 02:29:18 +0000

    File changes

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -113,8 +113,12 @@ export default function Map() {
         style={{ height: "100%" }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+          key={isDark ? "dark" : "light"}
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
+          url={isDark
+            ? "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            : "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+          }
         />
         {userLocation && <FlyToLocation coords={userLocation} />}
         {userLocation && (
